87ec48c1857380b099d242925153765e6430468a
Henry Agu <aguhenrychuks@gmail.com>
2025-01-18 19:04:17 +0100

    fix: image error

--- src/App.tsx
+++ src/App.tsx
@@ -8,32 +8,32 @@ const App = () => {
     {
       prefixes: ["0803", "0703", "0903", "0806", "0706", "0813", "0810", "0814", "0816", "0906", "0913", "091"],
       name: "MTN",
-      imgSrc: "/src/assets/mtn.jpg",
+      imgSrc: "/src/images/mtn.jpg",
     },
     {
       prefixes: ["0805", "0807", "0705", "0811", "0815", "0905", "0915"],
       name: "Glo",
-      imgSrc: "/src/assets/GloLogo.png",
+      imgSrc: "/src/images/GloLogo.png",
     },
     {
       prefixes: ["0802", "0808", "0708", "0701", "0812", "0901", "0902", "0904", "0907", "0912"],
       name: "Airtel",
-      imgSrc: "/src/assets/airtel.png",
+      imgSrc: "/src/images/airtel.png",
     },
     {
       prefixes: ["0809", "0817", "0818", "0908", "0909"],
       name: "9mobile",
-      imgSrc: "/src/assets/9mobile.jpg",
+      imgSrc: "/src/images/9mobile.jpg",
     },
     {
       prefixes: ["0804"],
       name: "Ntel",
-      imgSrc: "/src/assets/ntel.jpg",
+      imgSrc: "/src/images/ntel.jpg",
     },
     {
       prefixes: ["0819", "07028", "07029"],
       name: "Starcomms",
-      imgSrc: "/src/assets/starcomms.png",
+      imgSrc: "/src/images/starcomms.png",
     },
   ];
   
