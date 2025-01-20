bcf0ad7b1b84e9af3f43622027a86c2730f1f2d4
Henry Agu <aguhenrychuks@gmail.com>
2025-01-20 17:48:55 +0100

    feat: my new npm package & hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,106 +1,24 @@
 import { useState } from "react";
 import { toast, Toaster } from "sonner";
-
-interface NetworkData {
-  prefixes: string[];
-  name: string;
-  imgSrc: string;
-}
+import useVerifyNumber from "tel-check-ts";
 
 const App = () => {
   const [input, setInput] = useState<string>("");
 
-  const networkData: NetworkData[] = [
-    {
-      prefixes: [
-        "0803",
-        "0703",
-        "0903",
-        "0806",
-        "0706",
-        "0813",
-        "0810",
-        "0814",
-        "0816",
-        "0906",
-        "0913",
-      ],
-      name: "MTN",
-      imgSrc: "/images/mtn.jpg",
-    },
-    {
-      prefixes: ["0805", "0807", "0705", "0811", "0815", "0905", "0915"],
-      name: "Glo",
-      imgSrc: "/images/GloLogo.png",
-    },
-    {
-      prefixes: [
-        "0802",
-        "0808",
-        "0708",
-        "0701",
-        "0812",
-        "0901",
-        "0902",
-        "0904",
-        "0907",
-        "0912",
-        "0911",
-      ],
-      name: "Airtel",
-      imgSrc: "/images/airtel.png",
-    },
-    {
-      prefixes: ["0809", "0817", "0818", "0908", "0909"],
-      name: "9mobile",
-      imgSrc: "/images/9mobile.jpg",
-    },
-    {
-      prefixes: ["0804"],
-      name: "Ntel",
-      imgSrc: "/images/ntel.jpg",
-    },
-    {
-      prefixes: ["0819", "07028", "07029"],
-      name: "Starcomms",
-      imgSrc: "/images/starcomms.png",
-    },
-    {
-      prefixes: ["0702"],
-      name: "Smile",
-      imgSrc: "/images/smile.jpg",
-    },
-    {
-      prefixes: ["0707"],
-      name: "Zoom",
-      imgSrc: "/images/zoom.jpg",
-    },
-  ];
-
-  const verifyNumber = (input: string) => {
-    if (!input.trim()) {
-      toast("Please enter a phone number");
-      return;
-    }
-
-    const network = networkData.find((network) =>
-      network.prefixes.some((prefix) => input.startsWith(prefix))
-    );
-
-    if (network) {
-      toast(
-        <div className="flex items-center gap-x-3">
-          <img src={network.imgSrc} className="h-6 w-6" alt={network.name} />
-          {network.name === "9mobile" ||
-          network.name === "Starcomms" ||
-          network.name === "Zoom" ||
-          network.name === "Smile"
-            ? `This is a ${network.name} phone number.`
-            : `This is an ${network.name} phone number.`}
-        </div>
-      );
-    } else {
-      toast("Unknown network");
+  const handleVerification = () => {
+    const { verifyNumber } = useVerifyNumber();
+    const result = verifyNumber(input);
+    if (result) {
+      if (result.name && result.imgSrc) {
+        toast(
+          <div className="flex items-center gap-x-3">
+            <img src={result.imgSrc} className="h-6 w-6" alt={result.name} />
+            {result.message}
+          </div>
+        );
+      } else {
+        toast(result.message);
+      }
     }
   };
 
@@ -126,7 +44,7 @@ const App = () => {
         <button
           type="submit"
           className="bg-black p-4 rounded-full transition-all ease-out duration-200 hover:shadow-lg"
-          onClick={() => verifyNumber(input)}
+          onClick={handleVerification}
         >
           <svg
             width="18"
